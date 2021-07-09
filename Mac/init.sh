e540b492d7d21c1c1de129e8f5c4042e45ef394e
Katada, Junya <jkatada@gmail.com>
2021-07-09 11:13:48 +0900

    Polish

--- Mac/init.sh
+++ Mac/init.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Don't create .DS_Store file on network drive
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
